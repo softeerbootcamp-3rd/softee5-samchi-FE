3ce2b441c2a54ecdca3abb73b2b58d91e897c2c9
Jingeol Lee <trullerster@gmail.com>
2024-01-07 20:02:03 +0900

    테스트 파일 위치 조정

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -6,36 +6,36 @@ type Props = {
     selected:Array<boolean>,
     onSelect:React.Dispatch<React.SetStateAction<Array<boolean>>>,
     text:string,
+    style?:React.CSSProperties,
+    className?:string,
     children?: React.ReactNode
   }
   
 export const CheckBox = (props:Props) => {
 
   const clicked = () => {
-    const newSelected = props.selected.map((value, index) => {
-        if(index === props.index) return value = !value;
-        else return value;
-    });
+    const newSelected = 
+    props.selected.slice(0, props.index).concat( !props.selected[props.index], ...props.selected.slice(props.index+1));
     props.onSelect(newSelected);
     console.log(newSelected);
   }
   const stringCN = styles.rounded +' text-xl px-7 py-3 rounded-2xl border-solid border-gray-200 border-2 '+
   'transition-colors duration-150 focus-visible:duration-0 ';
-  //'hover:bg-gray-300 focus-visible:bg-gray-300',
-    //'focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus-visible:ring-gray-300',
-
+  
   return(
     <>
     {props.selected && props.selected[props.index] ?
       <button
         className={stringCN + `bg-black text-white`}
-        onClick={clicked} >
+        onClick={clicked} 
+        style={props.style}>
         <p>{props.text}</p>
         </button>
       : 
       <button 
         className={stringCN + `bg-white text-gray-600`}
-        onClick={clicked}> 
+        onClick={clicked}
+        style={props.style}> 
         <p>{props.text}</p>
         </button> 
     }
