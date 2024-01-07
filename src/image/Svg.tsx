02f47627c469336f7fb66528dd6dad8ead7a2560
Jingeol Lee <trullerster@gmail.com>
2024-01-07 23:37:10 +0900

    핀 svg

---
+++ src/image/Svg.tsx
@@ -0,0 +1,78 @@
+export const PinDriverGoal = (<svg width="42" height="44" viewBox="0 0 42 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_462_1388)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3379 8.08794C14.6353 5.79062 17.7511 4.5 21 4.5C24.2489 4.5 27.3647 5.79062 29.6621 8.08794C31.9594 10.3853 33.25 13.5011 33.25 16.75C33.25 25.9375 21 39.5 21 39.5C21 39.5 8.75 25.9375 8.75 16.75C8.75 13.5011 10.0406 10.3853 12.3379 8.08794Z" fill="#FF2E61"/>
+<path d="M16 18.8C16 18.8 16.625 18.2 18.5 18.2C20.375 18.2 21.625 19.4 23.5 19.4C25.375 19.4 26 18.8 26 18.8V11.6C26 11.6 25.375 12.2 23.5 12.2C21.625 12.2 20.375 11 18.5 11C16.625 11 16 11.6 16 11.6V18.8ZM16 18.8V23" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<filter id="filter0_d_462_1388" x="-4" y="-3" width="50" height="50" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_462_1388"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_462_1388" result="shape"/>
+</filter>
+</defs>
+</svg>
+);
+
+export const PinDriverSelected = (<svg width="42" height="44" viewBox="0 0 42 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_422_2732)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3379 8.08794C14.6353 5.79062 17.7511 4.5 21 4.5C24.2489 4.5 27.3647 5.79062 29.6621 8.08794C31.9594 10.3853 33.25 13.5011 33.25 16.75C33.25 25.9375 21 39.5 21 39.5C21 39.5 8.75 25.9375 8.75 16.75C8.75 13.5011 10.0406 10.3853 12.3379 8.08794Z" fill="#FF2E61"/>
+<path d="M27.1511 10.8838C26.9733 10.3675 26.4756 10 25.8889 10H16.1111C15.5244 10 15.0356 10.3675 14.8489 10.8838L13.0978 15.845C13.0356 16.0288 13 16.2213 13 16.4225V22.6875C13 23.4137 13.5956 24 14.3333 24C15.0711 24 15.6667 23.4137 15.6667 22.6875V22.25H26.3333V22.6875C26.3333 23.405 26.9289 24 27.6667 24C28.3956 24 29 23.4137 29 22.6875V16.4225C29 16.23 28.9644 16.0288 28.9022 15.845L27.1511 10.8838ZM16.1111 19.625C15.3733 19.625 14.7778 19.0387 14.7778 18.3125C14.7778 17.5862 15.3733 17 16.1111 17C16.8489 17 17.4444 17.5862 17.4444 18.3125C17.4444 19.0387 16.8489 19.625 16.1111 19.625ZM25.8889 19.625C25.1511 19.625 24.5556 19.0387 24.5556 18.3125C24.5556 17.5862 25.1511 17 25.8889 17C26.6267 17 27.2222 17.5862 27.2222 18.3125C27.2222 19.0387 26.6267 19.625 25.8889 19.625ZM14.7778 15.25L15.9067 11.9075C16.0311 11.5575 16.3689 11.3125 16.7511 11.3125H25.2489C25.6311 11.3125 25.9689 11.5575 26.0933 11.9075L27.2222 15.25H14.7778Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_422_2732" x="-4" y="-3" width="50" height="50" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_422_2732"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_422_2732" result="shape"/>
+</filter>
+</defs>
+</svg>);
+
+export const PinGuestDefault = (<svg width="42" height="44" viewBox="0 0 42 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_409_1562)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3379 8.08794C14.6353 5.79062 17.7511 4.5 21 4.5C24.2489 4.5 27.3647 5.79062 29.6621 8.08794C31.9594 10.3853 33.25 13.5011 33.25 16.75C33.25 25.9375 21 39.5 21 39.5C21 39.5 8.75 25.9375 8.75 16.75C8.75 13.5011 10.0406 10.3853 12.3379 8.08794Z" fill="#363636"/>
+<path d="M27.1511 10.8838C26.9733 10.3675 26.4756 10 25.8889 10H16.1111C15.5244 10 15.0356 10.3675 14.8489 10.8838L13.0978 15.845C13.0356 16.0288 13 16.2213 13 16.4225V22.6875C13 23.4137 13.5956 24 14.3333 24C15.0711 24 15.6667 23.4137 15.6667 22.6875V22.25H26.3333V22.6875C26.3333 23.405 26.9289 24 27.6667 24C28.3956 24 29 23.4137 29 22.6875V16.4225C29 16.23 28.9644 16.0288 28.9022 15.845L27.1511 10.8838ZM16.1111 19.625C15.3733 19.625 14.7778 19.0387 14.7778 18.3125C14.7778 17.5862 15.3733 17 16.1111 17C16.8489 17 17.4444 17.5862 17.4444 18.3125C17.4444 19.0387 16.8489 19.625 16.1111 19.625ZM25.8889 19.625C25.1511 19.625 24.5556 19.0387 24.5556 18.3125C24.5556 17.5862 25.1511 17 25.8889 17C26.6267 17 27.2222 17.5862 27.2222 18.3125C27.2222 19.0387 26.6267 19.625 25.8889 19.625ZM14.7778 15.25L15.9067 11.9075C16.0311 11.5575 16.3689 11.3125 16.7511 11.3125H25.2489C25.6311 11.3125 25.9689 11.5575 26.0933 11.9075L27.2222 15.25H14.7778Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_409_1562" x="-4" y="-3" width="50" height="50" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_409_1562"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_409_1562" result="shape"/>
+</filter>
+</defs>
+</svg>
+);
+
+export const PinGuestSelected = (<svg width="42" height="44" viewBox="0 0 42 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_409_1561)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12.3379 8.08794C14.6353 5.79062 17.7511 4.5 21 4.5C24.2489 4.5 27.3647 5.79062 29.6621 8.08794C31.9594 10.3853 33.25 13.5011 33.25 16.75C33.25 25.9375 21 39.5 21 39.5C21 39.5 8.75 25.9375 8.75 16.75C8.75 13.5011 10.0406 10.3853 12.3379 8.08794Z" fill="#FF2E61"/>
+<circle cx="21" cy="17" r="7" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_409_1561" x="-4" y="-3" width="50" height="50" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_409_1561"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_409_1561" result="shape"/>
+</filter>
+</defs>
+</svg>
+);
