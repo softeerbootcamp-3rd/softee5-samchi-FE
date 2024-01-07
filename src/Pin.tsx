5f6620f2c8190c5c18861ef9d1118aa1277e6fe0
Jingeol Lee <trullerster@gmail.com>
2024-01-07 19:22:28 +0900

    gitignore 업데이트

---
+++ src/Pin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export interface PinElementOptions {
+    type:string;
+    background?: string|null;
+    borderColor?: string|null;
+    glyph?: string|Element|URL|null;
+    glyphColor?: string|null;
+    scale?: number|null;
+  }
+export const Pin = () => {
+    
+}
