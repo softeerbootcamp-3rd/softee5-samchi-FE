f1b2c517944c1a377876ace5309580e081eb551e
Jingeol Lee <trullerster@gmail.com>
2024-01-22 00:58:19 +0900

    Change - NextButton에 disabled 속성 선택 추가

--- src/View/components/NextButton/NextButton.js
+++ src/View/components/NextButton/NextButton.js
@@ -1,24 +1,24 @@
 //NextButton과 그 파생형은 위치 잡을 필요 없음!
 //이미 CSS에서 잡아둠!
 
-export const NextButton = ({id = "", text}) => 
+export const NextButton = ({id = "", text, disabled = false}) => 
     `
-    <button class='box button next-button' ${id === '' ? '': 'id=' + id}>
+    <button class='box button next-button' ${id === '' ? '': 'id=' + id} ${disabled ? 'disabled' : ''}>
         <p>${text}</p>
     </button>
 `
 
-export const NextCircledNumberButton = ({id = "", text, number}) => 
+export const NextCircledNumberButton = ({id = "", text, number, disabled = false}) => 
     `
-    <button class='box button next-button' ${id === '' ? '': 'id=' + id}>
+    <button class='box button next-button' ${id === '' ? '': 'id=' + id} ${disabled ? 'disabled' : ''}>
         <p>${text}</p>
-        <p class='button-in-number'>${number}</p>
+        <p class='button-in-number' id='number-circle'>${number}</p>
     </button>
 `
 
-export const NextCancelButton = ({id = ""}) => 
+export const NextCancelButton = ({id = "", disabled = false}) => 
     `
-    <button class='box button next-button cancel-button' ${id === '' ? '': 'id=' + id}>
+    <button class='box button next-button cancel-button' ${id === '' ? '': 'id=' + id} ${disabled ? 'disabled' : ''}>
         <p>${text}</p>
     </button>
 `
